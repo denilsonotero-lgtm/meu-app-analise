1854e27659045e3797822a7fd56cdee7da7b7d1e
denilsonotero-lgtm <denilsonotero@gmail.com>
2026-08-18 14:48:01 -0300

    Update motor_inteligente.py

--- motor_inteligente.py
+++ motor_inteligente.py
@@ -1,39 +1,50 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 import os
+
+try:
+    from sklearn.metrics.pairwise import cosine_similarity
+    has_sklearn = True
+except ImportError:
+    has_sklearn = False
 
 def carregar_e_normalizar(ativo):
     filename = f'base_historica/{ativo.replace("=", "_").replace(".", "_")}.csv'
     if not os.path.exists(filename):
         return None
     
-    df = pd.read_csv(filename)
-    if 'Close' not in df.columns:
+    try:
+        df = pd.read_csv(filename)
+        if 'Close' not in df.columns:
+            return None
+        
+        precos = df['Close'].values
+        if len(precos) < 120:
+            return None
+            
+        tamanho_janela = 100
+        vetores = []
+        retornos_futuros = []
+        
+        for i in range(len(precos) - tamanho_janela - 1):
+            janela = precos[i : i + tamanho_janela]
+            janela_norm = (janela - janela[0]) / janela[0]
+            
+            preco_futuro = precos[i + tamanho_janela]
+            preco_atual_janela = janela[-1]
+            retorno = (preco_futuro - preco_atual_janela) / preco_atual_janela
+            
+            vetores.append(janela_norm)
+            retornos_futuros.append(retorno)
+            
+        return np.array(vetores), np.array(retornos_futuros)
+    except:
         return None
-    
-    precos = df['Close'].values
-    if len(precos) < 120:
-        return None
-        
-    tamanho_janela = 100
-    vetores = []
-    retornos_futuros = []
-    
-    for i in range(len(precos) - tamanho_janela - 1):
-        janela = precos[i : i + tamanho_janela]
-        janela_norm = (janela - janela[0]) / janela[0]
-        
-        preco_futuro = precos[i + tamanho_janela]
-        preco_atual_janela = janela[-1]
-        retorno = (preco_futuro - preco_atual_janela) / preco_atual_janela
-        
-        vetores.append(janela_norm)
-        retornos_futuros.append(retorno)
-        
-    return np.array(vetores), np.array(retornos_futuros)
 
 def prever_com_historico(df_atual, ativo):
+    if not has_sklearn:
+        return 50.0, 0
+        
     precos_atuais = df_atual['Close'].values[-100:]
     if len(precos_atuais) < 100:
         return 50.0, 0
